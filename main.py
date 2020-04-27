1a811387e32a284dd426e00bcbea1a4c5339668a
David <devid.nguyen21@gmail.com>
2020-04-27 22:36:00 +0300

    Add про gigagroup->Gigasafe

--- main.py
+++ main.py
@@ -50,12 +50,14 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def first_screen(message):
+        screen_item = Screen(bot, message)
+
         if message.text.lower() == 'про gigagroup':
-                mass = list(menu['ПРО GIGAGROUP'])
-                screen_item = Screen(bot, message)
-                screen_item.get_current_screen(mass, about_gigagroup, True)
-        elif message.text.lower() == 'связь с менеджером':
-                msg = bot.send_message(message.chat.id, 'Эта кнопка, пока еще в разработке', parse_mode="Markdown")
+            mass = list(menu['ПРО GIGAGROUP'])
+            screen_item.get_current_screen(mass, about_gigagroup, True)
+        elif message.text.lower() == 'топ-послуги':
+            mass = list(menu['ТОП-ПОСЛУГИ'])
+            screen_item.get_current_screen(mass, top_service, True)
         elif message.text.lower() == 'контакты':
                 msg = bot.send_message(message.chat.id, 'Наш адрес: адрес \nТелефон: телефон \nСайт: https://gigagroup.ua', parse_mode="Markdown")
         elif message.text.lower() == 'ответы на популярные вопросы':
@@ -78,7 +80,8 @@ def about_gigagroup(message):
             mass = list(menu['ПРО GIGAGROUP']['GIGACLOUD'])
             screen_item.get_current_screen(mass, about_gigacloud, True)
         elif message.text.lower() == 'gigasafe':
-            msg = bot.send_message(message.chat.id, 'Информация про групу компаний', reply_markup=keyboard_gigatrans, parse_mode="Markdown")
+            mass = list(menu['ПРО GIGAGROUP']['GIGASAFE'])
+            screen_item.get_current_screen(mass, about_gigasafe, True)
         elif message.text.lower() == 'назад':
             mass = list(menu.keys())
             screen_item.get_previous_screen(mass, first_screen, False, 'Почати')
@@ -88,23 +91,24 @@ def about_gigagroup(message):
         print(message.text)
 
 def about_gigatrans(message):
+    screen_item = Screen(bot, message)
+
     if message.text.lower() == 'послуги':
         mass = list(menu['ПРО GIGAGROUP']['GIGATRANS']['ПОСЛУГИ'])
-        screen_item = Screen(bot, message)
         screen_item.get_current_screen(mass, service_gigatrans, True)
     elif message.text.lower() == 'чому нас обирають':
         msg = bot.send_message(message.chat.id, dialog['ЧОМУ НАС ОБИРАЮТЬ GIGATRANS'], parse_mode="Markdown")
         bot.register_next_step_handler(msg, about_gigatrans)
     elif message.text.lower() == 'назад':
         mass = list(menu['ПРО GIGAGROUP'])
-        screen_item = Screen(bot, message)
         screen_item.get_previous_screen(mass, about_gigagroup, True, 'ПРО GIGAGROUP')
     else:
-        screen_item = Screen(bot, message)
         screen_item.get_error_screen(about_gigatrans)
     print(message.text)
 
 def service_gigatrans(message):
+    screen_item = Screen(bot, message)
+
     if message.text.lower() == 'інтернет для бізнесу':
         msg = bot.send_message(message.chat.id, dialog['ІНТЕРНЕТ ДЛЯ БІЗНЕСУ'], reply_markup=keyboard_service_order, parse_mode="Markdown")
         bot.register_next_step_handler(msg, service_order, 'GIGATRANS', 'ІНТЕРНЕТ ДЛЯ БІЗНЕСУ')
@@ -125,30 +129,29 @@ def service_gigatrans(message):
         bot.register_next_step_handler(msg, service_order, 'GIGATRANS', 'БУДІВНИЦТВО ВОЛЗ')
     elif message.text.lower() == 'назад':
         mass = list(menu['ПРО GIGAGROUP']['GIGATRANS'])
-        screen_item = Screen(bot, message)
         screen_item.get_previous_screen(mass, about_gigatrans, True, 'GIGATRANS')
     else:
-        screen_item = Screen(bot, message)
         screen_item.get_error_screen(service_gigatrans)
 
 def about_gigacenter(message):
+    screen_item = Screen(bot, message)
+
     if message.text.lower() == 'послуги':
         mass = list(menu['ПРО GIGAGROUP']['GIGACENTER']['ПОСЛУГИ'])
-        screen_item = Screen(bot, message)
         screen_item.get_current_screen(mass, service_gigacenter, True)
     elif message.text.lower() == 'чому нас обирають':
         msg = bot.send_message(message.chat.id, dialog['ЧОМУ НАС ОБИРАЮТЬ GIGACENTER'], parse_mode="Markdown")
         bot.register_next_step_handler(msg, about_gigacenter)
     elif message.text.lower() == 'назад':
         mass = list(menu['ПРО GIGAGROUP'])
-        screen_item = Screen(bot, message)
         screen_item.get_previous_screen(mass, about_gigagroup, True, 'ПРО GIGAGROUP')
     else:
-        screen_item = Screen(bot, message)
         screen_item.get_error_screen(about_gigacenter)
     print(message.text)
 
 def service_gigacenter(message):
+    screen_item = Screen(bot, message)
+
     if message.text.lower() == 'по-юнітне розміщення':
         msg = bot.send_message(message.chat.id, dialog['ПО-ЮНІТНЕ РОЗМІЩЕННЯ'], reply_markup=keyboard_service_order, parse_mode="Markdown")
         bot.register_next_step_handler(msg, service_order, 'GIGACENTER', 'ІНТЕРНЕТ ДЛЯ БІЗНЕСУ')
@@ -166,30 +169,29 @@ def service_gigacenter(message):
         bot.register_next_step_handler(msg, service_order, 'GIGACENTER', 'ІНТЕРНЕТ ДЛЯ БІЗНЕСУ')
     elif message.text.lower() == 'назад':
         mass = list(menu['ПРО GIGAGROUP']['GIGACENTER'])
-        screen_item = Screen(bot, message)
         screen_item.get_previous_screen(mass, about_gigacenter, True, 'GIGACENTER')
     else:
-        screen_item = Screen(bot, message)
         screen_item.get_error_screen(service_gigacenter)
 
 def about_gigacloud(message):
+    screen_item = Screen(bot, message)
+
     if message.text.lower() == 'послуги':
         mass = list(menu['ПРО GIGAGROUP']['GIGACLOUD']['ПОСЛУГИ'])
-        screen_item = Screen(bot, message)
         screen_item.get_current_screen(mass, service_gigacloud, True)
     elif message.text.lower() == 'чому нас обирають':
         msg = bot.send_message(message.chat.id, dialog['ЧОМУ НАС ОБИРАЮТЬ GIGACLOUD'], parse_mode="Markdown")
         bot.register_next_step_handler(msg, about_gigacloud)
     elif message.text.lower() == 'назад':
         mass = list(menu['ПРО GIGAGROUP'])
-        screen_item = Screen(bot, message)
         screen_item.get_previous_screen(mass, about_gigagroup, True, 'ПРО GIGAGROUP')
     else:
-        screen_item = Screen(bot, message)
         screen_item.get_error_screen(about_gigacloud)
     print(message.text)
 
 def service_gigacloud(message):
+    screen_item = Screen(bot, message)
+
     if message.text.lower() == 'private cloud':
         msg = bot.send_message(message.chat.id, dialog['PRIVATE CLOUD'], reply_markup=keyboard_service_order, parse_mode="Markdown")
         bot.register_next_step_handler(msg, service_order, 'GIGACLOUD', 'ІНТЕРНЕТ ДЛЯ БІЗНЕСУ')
@@ -207,42 +209,138 @@ def service_gigacloud(message):
         bot.register_next_step_handler(msg, service_order, 'GIGACLOUD', 'ІНТЕРНЕТ ДЛЯ БІЗНЕСУ')
     elif message.text.lower() == 'назад':
         mass = list(menu['ПРО GIGAGROUP']['GIGACLOUD'])
-        screen_item = Screen(bot, message)
-        screen_item.get_previous_screen(mass, about_gigacenter, True, 'GIGACLOUD')
+        screen_item.get_previous_screen(mass, about_gigacloud, True, 'GIGACLOUD')
     else:
-        screen_item = Screen(bot, message)
         screen_item.get_error_screen(service_gigacloud)
 
+def about_gigasafe(message):
+    screen_item = Screen(bot, message)
+
+    if message.text.lower() == 'послуги':
+        mass = list(menu['ПРО GIGAGROUP']['GIGASAFE']['ПОСЛУГИ'])
+        screen_item.get_current_screen(mass, service_gigasafe, True)
+    elif message.text.lower() == 'чому нас обирають':
+        msg = bot.send_message(message.chat.id, dialog['ЧОМУ НАС ОБИРАЮТЬ GIGASAFE'], parse_mode="Markdown")
+        bot.register_next_step_handler(msg, about_gigasafe)
+    elif message.text.lower() == 'назад':
+        mass = list(menu['ПРО GIGAGROUP'])
+        screen_item.get_previous_screen(mass, about_gigagroup, True, 'ПРО GIGAGROUP')
+    else:
+        screen_item.get_error_screen(about_gigasafe)
+    print(message.text)
+
+def service_gigasafe(message):
+    screen_item = Screen(bot, message)
+
+    if message.text.lower() == 'ідентифікація загроз':
+        msg = bot.send_message(message.chat.id, dialog['ІДЕНТИФІКАЦІЯ ЗАГРОЗ'], reply_markup=keyboard_service_order, parse_mode="Markdown")
+        bot.register_next_step_handler(msg, service_order, 'GIGASAFE', 'ІНТЕРНЕТ ДЛЯ БІЗНЕСУ')
+    elif message.text.lower() == 'налаштування захисту':
+        msg = bot.send_message(message.chat.id, dialog['НАЛАШТУВАННЯ ЗАХИСТУ'], reply_markup=keyboard_service_order, parse_mode="Markdown")
+        bot.register_next_step_handler(msg, service_order, 'GIGASAFE', 'ІНТЕРНЕТ ДЛЯ БІЗНЕСУ')
+    elif message.text.lower() == 'моніторинг аномалій':
+        msg = bot.send_message(message.chat.id, dialog['МОНІТОРИНГ АНОМАЛІЙ'], reply_markup=keyboard_service_order,parse_mode="Markdown")
+        bot.register_next_step_handler(msg, service_order, 'GIGASAFE', 'ІНТЕРНЕТ ДЛЯ БІЗНЕСУ')
+    elif message.text.lower() == 'реагування на інциденти':
+        msg = bot.send_message(message.chat.id, dialog['РЕАГУВАННЯ НА ІНЦИДЕНТИ'], reply_markup=keyboard_service_order,parse_mode="Markdown")
+        bot.register_next_step_handler(msg, service_order, 'GIGASAFE', 'ІНТЕРНЕТ ДЛЯ БІЗНЕСУ')
+    elif message.text.lower() == 'відновлення на покращення':
+        msg = bot.send_message(message.chat.id, dialog['ВІДНОВЛЕННЯ НА ПОКРАЩЕННЯ'], reply_markup=keyboard_service_order,parse_mode="Markdown")
+        bot.register_next_step_handler(msg, service_order, 'GIGASAFE', 'ІНТЕРНЕТ ДЛЯ БІЗНЕСУ')
+    elif message.text.lower() == 'назад':
+        mass = list(menu['ПРО GIGAGROUP']['GIGASAFE'])
+        screen_item.get_previous_screen(mass, about_gigasafe, True, 'GIGASAFE')
+    else:
+        screen_item.get_error_screen(service_gigasafe)
 
 def service_order(message, company, service):
     print(company)
+    screen_item = Screen(bot, message)
+
     if company == 'GIGATRANS':
         if message.text.lower() == 'замовити послугу':
             mass = list(menu['ПРО GIGAGROUP'])
-            screen_item = Screen(bot, message)
             screen_item.get_previous_screen(mass, about_gigagroup, True, 'ПРО GIGAGROUP')
         else:
             mass = list(menu['ПРО GIGAGROUP']['GIGATRANS']['ПОСЛУГИ'])
-            screen_item = Screen(bot, message)
             screen_item.get_previous_screen(mass, service_gigatrans, True, 'ПОСЛУГИ')
     elif company == 'GIGACENTER':
         if message.text.lower() == 'замовити послугу':
             mass = list(menu['ПРО GIGAGROUP'])
-            screen_item = Screen(bot, message)
             screen_item.get_previous_screen(mass, about_gigagroup, True, 'ПРО GIGAGROUP')
         else:
             mass = list(menu['ПРО GIGAGROUP']['GIGACENTER']['ПОСЛУГИ'])
-            screen_item = Screen(bot, message)
             screen_item.get_previous_screen(mass, service_gigacenter, True, 'ПОСЛУГИ GIGACENTER')
     elif company == 'GIGACLOUD':
         if message.text.lower() == 'замовити послугу':
             mass = list(menu['ПРО GIGAGROUP'])
-            screen_item = Screen(bot, message)
             screen_item.get_previous_screen(mass, about_gigagroup, True, 'ПРО GIGAGROUP')
         else:
             mass = list(menu['ПРО GIGAGROUP']['GIGACLOUD']['ПОСЛУГИ'])
-            screen_item = Screen(bot, message)
             screen_item.get_previous_screen(mass, service_gigacloud, True, 'ПОСЛУГИ')
+    elif company == 'GIGASAFE':
+        if message.text.lower() == 'замовити послугу':
+            mass = list(menu['ПРО GIGAGROUP'])
+            screen_item.get_previous_screen(mass, about_gigagroup, True, 'ПРО GIGAGROUP')
+        else:
+            mass = list(menu['ПРО GIGAGROUP']['GIGASAFE']['ПОСЛУГИ'])
+            screen_item.get_previous_screen(mass, service_gigasafe, True, 'ПОСЛУГИ')
 
+def top_service(message):
+    screen_item = Screen(bot, message)
+
+    if message.text.lower() == 'інтернет для бізнесу':
+        msg = bot.send_message(message.chat.id, dialog['ІНТЕРНЕТ ДЛЯ БІЗНЕСУ'], reply_markup=keyboard_service_order,parse_mode="Markdown")
+        bot.register_next_step_handler(msg, service_top_order, 'GIGATRANS', 'ІНТЕРНЕТ ДЛЯ БІЗНЕСУ')
+    elif message.text.lower() == 'оренда екранованої шафи':
+        msg = bot.send_message(message.chat.id, dialog['ОРЕНДА ЕКРАНОВАНОЇ ШАФИ'], reply_markup=keyboard_service_order,parse_mode="Markdown")
+        bot.register_next_step_handler(msg, service_top_order, 'GIGACENTER', 'ОРЕНДА ЕКРАНОВАНОЇ ШАФИ')
+    elif message.text.lower() == 'private cloud':
+        msg = bot.send_message(message.chat.id, dialog['PRIVATE CLOUD'], reply_markup=keyboard_service_order, parse_mode="Markdown")
+        bot.register_next_step_handler(msg, service_top_order, 'GIGACLOUD', 'PRIVATE CLOUD')
+    elif message.text.lower() == 'гібридні рішення':
+        msg = bot.send_message(message.chat.id, dialog['ГІБРИДНІ РІШЕННЯ'], reply_markup=keyboard_service_order,parse_mode="Markdown")
+        bot.register_next_step_handler(msg, service_top_order, 'GIGACLOUD', 'ГІБРИДНІ РІШЕННЯ')
+    elif message.text.lower() == 'ідентифікація загроз':
+        msg = bot.send_message(message.chat.id, dialog['ІДЕНТИФІКАЦІЯ ЗАГРОЗ'], reply_markup=keyboard_service_order, parse_mode="Markdown")
+        bot.register_next_step_handler(msg, service_top_order, 'GIGASAFE', 'ІДЕНТИФІКАЦІЯ ЗАГРОЗ')
+    elif message.text.lower() == 'будівництво волз':
+        msg = bot.send_message(message.chat.id, dialog['БУДІВНИЦТВО ВОЛЗ'], reply_markup=keyboard_service_order,parse_mode="Markdown")
+        bot.register_next_step_handler(msg, service_top_order, 'GIGATRANS', 'БУДІВНИЦТВО ВОЛЗ')
+    elif message.text.lower() == 'назад':
+        mass = list(menu.keys())
+        screen_item.get_previous_screen(mass, first_screen, False, 'Почати')
+    else:
+        screen_item.get_error_screen(top_service)
+
+
+def service_top_order(message, company, service):
+    print(company)
+    screen_item = Screen(bot, message)
+
+    if message.text.lower() == 'замовити послугу':
+        if service == 'ІНТЕРНЕТ ДЛЯ БІЗНЕСУ':
+            mass = list(menu['ТОП-ПОСЛУГИ'])
+            screen_item.get_previous_screen(mass, top_service, True, 'ТОП-ПОСЛУГИ')
+        elif service == 'ОРЕНДА ЕКРАНОВАНОЇ ШАФИ':
+            mass = list(menu['ТОП-ПОСЛУГИ'])
+            screen_item.get_previous_screen(mass, top_service, True, 'ТОП-ПОСЛУГИ')
+        elif service == 'PRIVATE CLOUD':
+            mass = list(menu['ТОП-ПОСЛУГИ'])
+            screen_item.get_previous_screen(mass, top_service, True, 'ТОП-ПОСЛУГИ')
+        elif service == 'ГІБРИДНІ РІШЕННЯ':
+            mass = list(menu['ТОП-ПОСЛУГИ'])
+            screen_item.get_previous_screen(mass, top_service, True, 'ТОП-ПОСЛУГИ')
+        elif service == 'ІДЕНТИФІКАЦІЯ ЗАГРОЗ':
+            mass = list(menu['ТОП-ПОСЛУГИ'])
+            screen_item.get_previous_screen(mass, top_service, True, 'ТОП-ПОСЛУГИ')
+        elif service == 'БУДІВНИЦТВО ВОЛЗ':
+            mass = list(menu['ТОП-ПОСЛУГИ'])
+            screen_item.get_previous_screen(mass, top_service, True, 'ТОП-ПОСЛУГИ')
+    elif message.text.lower() == 'назад':
+        mass = list(menu['ТОП-ПОСЛУГИ'])
+        screen_item.get_previous_screen(mass, top_service, True, 'ТОП-ПОСЛУГИ')
+    else:
+        screen_item.get_error_screen(service_top_order)
 
 bot.polling()
